5767baaf5bb69b7fdd30d47cd737d37cf4f2f7ff
Cristhian <crijpardomer@unal.edu.co>
2025-07-23 00:51:26 -0500

    api and db tests

---
+++ api-db-tests.sh
@@ -0,0 +1,5 @@
+# Health general
+curl -i https://ckemxpwktk.execute-api.us-east-1.amazonaws.com/Prod/ping
+
+# Health de DB
+curl -i https://ckemxpwktk.execute-api.us-east-1.amazonaws.com/Prod/health/db
